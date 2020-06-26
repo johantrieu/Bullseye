13919a1bd070faf78fb0bdc7d50f887ede236bdc
johantrieu <johantrieu@gmail.com>
2020-06-26 19:12:55 +0300

    initial commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by zalukana on 26.6.2020.
+//  Copyright © 2020 zalukana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to my first app!")
+            .fontWeight(.semibold)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
