d83a44b5ad07a51edd24d57efb64e4b781200f19
johantrieu <johantrieu@gmail.com>
2020-06-27 20:46:22 +0300

    update button and alerts

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,9 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
     var body: some View {
-        Text("Welcome to my first app!")
-            .fontWeight(.semibold)
+        VStack {
+            VStack {
+                Text("Welcome to my first app!")
+                    .fontWeight(.black).foregroundColor(.green)
+            }
+            Button(action: {
+                print("Button press!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit Me")
+            }
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert(title: Text("Hello there"),
+                      message: Text("This is my first pop-up."),
+                      dismissButton: .default(Text("Awesome!")))
+            }
+        }
     }
 }
 
