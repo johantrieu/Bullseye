14d9e3c8d59a8882cee0951cd28a06cd5a119e99
johantrieu <johantrieu@gmail.com>
2020-06-28 22:39:52 +0300

    add Spacer

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,14 +8,33 @@
 
 import SwiftUI
 
+// Properties
+// ==========
+
+// User interface views
+
 struct ContentView: View {
     @State var alertIsVisible: Bool = false
+    
+    // User interface content and layout
     var body: some View {
         VStack {
-            VStack {
-                Text("Welcome to my first app!")
-                    .fontWeight(.black).foregroundColor(.green)
+            
+            // Target now
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
             }
+            
+            // Slider row
+            HStack {
+            Text("1")
+                Slider(value: .constant(10))
+            Text("100")
+            }
+            
+            // Button
+            
             Button(action: {
                 print("Button press!")
                 self.alertIsVisible = true
@@ -27,12 +46,38 @@ struct ContentView: View {
                       message: Text("This is my first pop-up."),
                       dismissButton: .default(Text("Awesome!")))
             }
+            
+            // Score row
+                
+            HStack {
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
         }
     }
+    // Methods
+    // =======
 }
+
+// Preview
+// =======
+
+#if DEBUG
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
